7afd734b5b1d988430e4b8a4c80ae847288a30a6
DP5_deviaanjani <deviaanjani82gmail.com>
2024-05-19 20:02:47 +0700

    commit Kedua

--- src/Soal1/Main.java
+++ src/Soal1/Main.java
@@ -1,4 +1,9 @@
 package Soal1;
 
-public class Main {
+import java.util.Scanner; //mengimpor kelass Scanner dari paket java.until untuk membaca input dari user
+public class Main { //deklarasi kelas utama dengan nama Main
+    public static void main(String[] args) { //Deklarasi metode main, titik awal eksekusi program
+        Scanner scanner = new Scanner(System.in);//membuat objek Scanner untuk membacainput dari konsol
+        Perpustakaan perpustakaan = new Perpustakaan();//Membuat objek Perpustakaan untuk mengelola koleksi buku dan majalah
+    }
 }
--- src/Soal1/Perpustakaan.java
+++ src/Soal1/Perpustakaan.java
@@ -1,4 +1,15 @@
 package Soal1;
 
-public class Perpustakaan {
+import java.util.ArrayList;//Mengimpor kelas ArrayList dari paket java.util untuk membuat daftar dinamis
+class Buku { //Deklarasi kelas Buku yang mewakili sebuah buku
+    private String judul;//Deklarasi atribut judul dengan tipe data String
+    private String pengarang;//Deklarasi atribut pengarang dengan tipe data String
+    private int tahunTerbit;//Deklarasi atribut tahunTerbit dengan tipe data int
+
+    // Constructor untuk menginisialisasi objek Buku dengan judul, pengarang, dan tahun terbit
+    public Buku(String judul, String pengarang, int tahunTerbit) {
+        this.judul = judul; //Menginisialisasi atribut judul dengan nilai parameter judul
+        this.pengarang = pengarang; //Menginisialisasi atribut pengarang dengan nilai parameter pengarang
+        this.tahunTerbit = tahunTerbit; //Menginisialisasi atribut tahunTerbit dengan nilai parameter tahunTerbit
+    }
 }
