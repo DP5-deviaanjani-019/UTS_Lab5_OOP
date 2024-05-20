49f37662d7ae98a87a9881bece1023128af3c249
DP5_deviaanjani <deviaanjani82gmail.com>
2024-05-20 11:57:53 +0700

    Commit Kedelapan

--- src/Soal2/Main.java
+++ src/Soal2/Main.java
@@ -4,14 +4,21 @@ import java.util.Scanner;
  public class Main {
      public static void main(String[] args){
          Scanner scanner = new Scanner(System.in);
-         System.out.print("Masukkan sebuah string: ");
-         String inputString = scanner.nextLine();
-         boolean result = PalindromeChecker.isPalindrome(inputString);
-         System.out.println("Apakah string ini palindrome? " + result);
+         boolean keepRunning = true;
+         while (keepRunning) {
+             System.out.print("Masukkan sebuah string / ketik 'keluar' untuk berhenti :");
+             String inputString = scanner.nextLine();
+             if (inputString.equalsIgnoreCase("keluar")) {
+                 keepRunning = false;
+             } else {
+                 boolean result = PalindromeChecker.isPalindrome(inputString);
+                 System.out.println("Apakah string ini palindrome? " + result);
+             }
+         }
          scanner.close();
 
 
      }
 
-    }
+ }
 
--- src/Soal2/PalindromeChecker.java
+++ src/Soal2/PalindromeChecker.java
@@ -1,5 +1,4 @@
 package Soal2;
-
 public class PalindromeChecker {
     public static boolean isPalindrome(String str) {
         String cleanedString = str.replaceAll("\\s+", "").toLowerCase();
