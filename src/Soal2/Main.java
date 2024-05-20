2935ba5484f84992219c3905b286821e737b4687
DP5_deviaanjani <deviaanjani82gmail.com>
2024-05-20 11:28:09 +0700

    Commit Kedua

--- src/Soal2/Main.java
+++ src/Soal2/Main.java
@@ -1,5 +1,11 @@
 package Soal2;
+
+import java.util.Scanner;
  public class Main {
+     public static void main(String[] args){
+         Scanner scanner = new Scanner(System.in);
+
+     }
 
     }
 
--- src/Soal2/PalindromeChecker.java
+++ src/Soal2/PalindromeChecker.java
@@ -1,5 +1,8 @@
 package Soal2;
 
 public class PalindromeChecker {
-
+    public static boolean isPalindrome(String str) {
+        String cleanedString = str.replaceAll("\\s+", "").toLowerCase();
+        int left = 0;
+    }
 }
