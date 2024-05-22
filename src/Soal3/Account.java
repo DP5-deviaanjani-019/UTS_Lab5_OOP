ac0736492ce1022bc4fca31ca8b85ff276283043
DP5_deviaanjani <deviaanjani82gmail.com>
2024-05-22 09:34:00 +0700

    Commit Pertama

---
+++ src/Soal3/Account.java
@@ -0,0 +1,10 @@
+package Soal3;
+
+public class Account {
+    private String id;
+    private String name;
+    private int balance;
+
+    public Account(String id, String name, int balance) {
+    }
+}
---
+++ src/Soal3/TestMain.java
@@ -0,0 +1,7 @@
+package Soal3;
+public class TestMain {
+    public static void main(String[] args) {
+        Account a1 = new Account("A101", "Ligma", 88);
+        System.out.println(a1);
+    }
+    }
